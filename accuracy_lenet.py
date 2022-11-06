1866eb0a00a145656bb97b4dd61f079c63d3dc45
Mai Chí Bảo <mrzaizai2k@gmail.com>
2022-11-06 10:44:15 +0700

    Add files via upload

---
+++ accuracy_lenet.py
@@ -0,0 +1,35 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import pandas as pd
+import seaborn as sns
+### Confusion Matrix
+from sklearn.metrics import confusion_matrix
+
+'''File này tiện lợi cho việc lưu trữ kết quả và hiển thị Accuracy'''
+
+class_names = ['BC-15', 'Huongthom', 'Nep87', 'Q5', 'Thien_uu', 'Xi23']
+
+df = pd.read_csv('test_path_and_y_lenet.csv') # Đọc file csv chứa y_true và y_predict
+y_predict = df.values[:, -1] # Đọc giá trị predict
+y_true = df.values[:, 1] # Đọc giá trị y_true
+
+cm = confusion_matrix(y_true, y_predict) # Tạo confusion matrix
+# Plot confusion matrix in a beautiful manner
+fig = plt.figure(figsize=(16, 14))
+ax = plt.subplot()
+sns.heatmap(cm, annot=True, ax=ax, fmt='g');  # annot=True to annotate cells
+# labels, title and ticks
+ax.set_xlabel('Predicted', fontsize=20)
+ax.xaxis.set_label_position('bottom')
+plt.xticks(rotation=90)
+ax.xaxis.set_ticklabels(class_names, fontsize=10)
+ax.xaxis.tick_bottom()
+
+ax.set_ylabel('True', fontsize=20)
+ax.yaxis.set_ticklabels(class_names, fontsize=10)
+plt.yticks(rotation=0)
+
+plt.title('Rice Confusion Matrix Lenet', fontsize=20) # Tiêu đề
+
+plt.savefig('Accuracy Lenet.png') # Lưu ảnh
+plt.show()
---
+++ test_folder_csv_resnet.py
@@ -0,0 +1,76 @@
+import csv
+import os
+
+import numpy as np
+import tensorflow as tf
+from tensorflow.keras import layers
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.models import Model
+from tensorflow.keras.layers import Dense, Flatten, GlobalAveragePooling2D, BatchNormalization, Dropout
+from tensorflow.keras.applications.resnet import ResNet50
+
+''' File này dùng để predict trên hết folder test để đưa ra ảnh accuracy
+Note: Trước khi chạy file nhớ đổi tên file csv, vì khi chạy nó sẽ ghi đè
+'''
+### Load Model ###
+
+model_dir = 'model/2022-11-04/rice_model_resnet_12.h5' # Đường dẫn model
+
+# Khởi tạo thông số ảnh và model
+img_height = 100
+img_width = 200
+num_classes = 6
+
+resnet = ResNet50(weights= 'imagenet', include_top=False, input_shape= (img_height,img_width,3))
+
+x = resnet.output
+x = GlobalAveragePooling2D()(x)
+x = Dense(512, activation = "relu")(x)
+x = Dropout(0.3)(x)#
+x = Dense(256, activation = "relu")(x)
+x = Dropout(0.3)(x)
+predictions = Dense(num_classes, activation= 'softmax')(x)
+model = Model(inputs = resnet.input, outputs = predictions)
+
+model.load_weights(model_dir)
+class_names = ['BC-15', 'Huongthom', 'Nep87', 'Q5', 'Thien_uu', 'Xi23']
+
+# Đọc folder ảnh cần predict
+test_set_dir = 'data/rice_test'
+
+# Header cho file csv
+header = 'path y_true y_predict'
+header = header.split()
+
+file = open('test_path_and_y_resnet.csv', 'w') # Đổi tên chỗ này, tên mới sẽ tạo file mới, tên cũ thì ghi đè
+with file:
+    writer = csv.writer(file)
+    writer.writerow(header)
+data = []
+
+# Bắt đầu đọc từng folder, rồi từng folder lại đọc tiếp từng ảnh, y_true là tên folder, y_predict là từ model
+for rice_folder in os.listdir(test_set_dir):
+    rice_folder_dir = os.path.join(test_set_dir, rice_folder) # Đường dẫn folder gạo
+    print('rice_folder', rice_folder)
+    for rice_img in os.listdir(rice_folder_dir):
+        rice_img_dir = os.path.join(rice_folder_dir, rice_img) # Đường dẫn ảnh hạt gạo
+
+        y_true = str(rice_folder) # y_true là tên folder
+
+        img = tf.keras.utils.load_img(
+            rice_img_dir, target_size=(img_height, img_width)
+        )
+        img_array = tf.keras.utils.img_to_array(img)
+        img_array = tf.expand_dims(img_array, 0)  # Create a batch
+
+        predictions = model.predict(img_array)
+        score = tf.nn.softmax(predictions[0])
+        y_predict = str(class_names[np.argmax(score)]) # y_predict của model
+
+        # Mở file ra rồi chạy lại
+        data = [rice_img_dir, y_true, y_predict]
+        file = open('test_path_and_y_resnet.csv', 'a')
+        with file:
+            writer = csv.writer(file)
+            writer.writerow(data)
+
